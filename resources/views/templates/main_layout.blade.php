75d14b36fe22b232613d6bc8d8c8c643bd8e9d74
Joao Ribeiro <business@sys4soft.com>
2023-09-16 22:04:41 +0100

    021

--- resources/views/templates/main_layout.blade.php
+++ resources/views/templates/main_layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 </head>
 <body>
     
